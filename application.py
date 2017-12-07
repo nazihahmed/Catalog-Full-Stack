4d81d19ca30a2b59bb46dd35c988b6fc1fd54c62
maxpcs <max.pcs111@gmail.com>
2017-12-07 20:19:04 +0200

    added client secrets file

--- application.py
+++ application.py
@@ -24,8 +24,7 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 app = Flask(__name__)
 
-CLIENT_ID = '149573992398-133jfmfajp34d20ea65dj31phur2onbb.apps.googleusercontent.com'# json.loads(
-   # open('client_secret.json', 'r').read())['web']['client_id']
+CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
 
 
 # CRUD Functionality
@@ -104,14 +103,11 @@ def verify_password(username_or_token, password):
     return True
 
 @app.route('/')
+@app.route('/catalog')
 def default():
     categories = showCategories()
     items = showItems(None,10)
     return render_template('catalog.html',categories = categories, items=items)
-
-@app.route('/clientOAuth')
-def start():
-    return render_template('clientOAuth.html')
 
 @app.route('/catalog/<categoryName>')
 def categoryDisplay(categoryName):
@@ -206,7 +202,7 @@ def categoryItemEdit(categoryName,itemName):
 def categoryItemDelete(categoryName,itemName):
         it = showItem(categoryName,itemName)
         return render_template('category_item_delete.html',item=it)
-        
+
 @auth.login_required
 @app.route('/catalog/<categoryName>/<itemName>/deleteConfirm')
 #@auth.login_required
@@ -222,102 +218,14 @@ def categoryItemDeleteConfirm(categoryName,itemName):
             flash(u'Server Error, please try again later','danger')
             return redirect(url_for('categoryDisplay',categoryName=categoryName))
 
-@app.route('/oauth/<provider>', methods = ['POST'])
-def login(provider):
-    #STEP 1 - Parse the auth code
-    auth_code = request.json.get('auth_code')
-    print "Step 1 - Complete, received auth code %s" % auth_code
-    if provider == 'google':
-        #STEP 2 - Exchange for a token
-        try:
-            # Upgrade the authorization code into a credentials object
-            oauth_flow = flow_from_clientsecrets('client_secret.json', scope='')
-            oauth_flow.redirect_uri = 'postmessage'
-            credentials = oauth_flow.step2_exchange(auth_code)
-        except FlowExchangeError:
-            response = make_response(json.dumps('Failed to upgrade the authorization code.'), 401)
-            response.headers['Content-Type'] = 'application/json'
-            return response
-
-        # Check that the access token is valid.
-        access_token = credentials.access_token
-        url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
-        h = httplib2.Http()
-        result = json.loads(h.request(url, 'GET')[1])
-        # If there was an error in the access token info, abort.
-        if result.get('error') is not None:
-            response = make_response(json.dumps(result.get('error')), 500)
-            response.headers['Content-Type'] = 'application/json'
-
-        # # Verify that the access token is used for the intended user.
-        # gplus_id = credentials.id_token['sub']
-        # if result['user_id'] != gplus_id:
-        #     response = make_response(json.dumps("Token's user ID doesn't match given user ID."), 401)
-        #     response.headers['Content-Type'] = 'application/json'
-        #     return response
-
-        # # Verify that the access token is valid for this app.
-        # if result['issued_to'] != CLIENT_ID:
-        #     response = make_response(json.dumps("Token's client ID does not match app's."), 401)
-        #     response.headers['Content-Type'] = 'application/json'
-        #     return response
-
-        # stored_credentials = login_session.get('credentials')
-        # stored_gplus_id = login_session.get('gplus_id')
-        # if stored_credentials is not None and gplus_id == stored_gplus_id:
-        #     response = make_response(json.dumps('Current user is already connected.'), 200)
-        #     response.headers['Content-Type'] = 'application/json'
-        #     return response
-        print "Step 2 Complete! Access Token : %s " % credentials.access_token
-
-        #STEP 3 - Find User or make a new one
-
-        #Get user info
-        h = httplib2.Http()
-        userinfo_url =  "https://www.googleapis.com/oauth2/v1/userinfo"
-        params = {'access_token': credentials.access_token, 'alt':'json'}
-        answer = requests.get(userinfo_url, params=params)
-
-        data = answer.json()
-
-        name = data['name']
-        picture = data['picture']
-        email = data['email']
-
-
-
-        #see if user exists, if it doesn't make a new one
-        user = session.query(User).filter_by(email=email).first()
-        if not user:
-            user = User(username = name, picture = picture, email = email)
-            session.add(user)
-            session.commit()
-
-
-
-        #STEP 4 - Make token
-        token = user.generate_auth_token(600)
-
-
-
-        #STEP 5 - Send back token to the client
-        return jsonify({'token': token.decode('ascii')})
-
-        #return jsonify({'token': token.decode('ascii'), 'duration': 600})
-    else:
-        return 'Unrecoginized Provider'
-
-@app.route('/token')
-@auth.login_required
-def get_auth_token():
-    token = g.user.generate_auth_token()
-    return jsonify({'token': token.decode('ascii')})
-
+# Create anti-forgery state token
 @app.route('/login')
-def loginPage():
-    return render_template('login.html',client_id=CLIENT_ID)
-
-
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32))
+    login_session['state'] = state
+    # return "The current session state is %s" % login_session['state']
+    return render_template('login.html', STATE=state)
 
 @app.route('/users', methods = ['POST'])
 def new_user():
